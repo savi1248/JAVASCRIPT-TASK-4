3cde86ace549aac47f4a2b3a69b2b54c4689675b
savitha12 <savithaa24082000@gmail.com>
2023-08-02 16:27:49 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const input = document.getElementById("input")
+const addBtn = document.getElementById("addBtn")
+const list = document.getElementById('list')
+
+
+
+addBtn.addEventListener('click',() => {
+
+    if(input.value == '' ) {
+        input.style.border = '1px solid red'
+    } 
+    else{
+
+        const li = document.createElement('li')
+        const span = document.createElement('span')
+        const p = document.createElement('p')
+        
+        
+        p.className = ''
+        p.innerText = input.value
+
+    span.innerText = 'delete'
+    span.addEventListener('click',() => {
+        list.removeChild(li)
+    })
+    
+    li.addEventListener('click',() => {
+        p.classList.toggle('checked')
+    })
+
+    li.appendChild(p)
+    li.appendChild(span)
+    list.appendChild(li)
+
+    input.style.border = '1px solid white'
+    input.value = ''
+}
+})
